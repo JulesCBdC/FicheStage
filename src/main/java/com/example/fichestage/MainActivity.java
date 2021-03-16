05b8380b94031f216fa7f72262f60eb778c88b00
JulesCBdC <jcarlier@la-joliverie>
2021-03-16 23:01:57 +0100

    dépot 1

---
+++ src/main/java/com/example/fichestage/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.fichestage;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    ListView mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.btnAjouter);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity1();
+            }
+        });
+    }
+    public void openActivity1() {
+        Intent intent = new Intent(this, etape1Activity.class);
+        startActivity(intent);
+    }
+}
